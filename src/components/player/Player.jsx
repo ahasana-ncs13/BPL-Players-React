7447c79f6a1bda8d30ad5476d1a85c20d02c8338
ahasana-ncs13 <ahasanancs0485@gmail.com>
2025-09-23 14:49:06 +0600

    added card

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
 const Player = ({eachPlayer}) => {
-    console.log(eachPlayer)
+    // console.log(eachPlayer)
     return (
-        <div>
-            <img src="" alt="" />
+        <div className='p-5 border border-gray-200 rounded-2xl  '>
+        <img src={eachPlayer.image} alt="" className='w-100 h-70 rounded-2xl' />
+        <h2 className='text-xl font-bold my-4'>{eachPlayer.name}</h2>
+
+        <div className="flex justify-between items-center mb-4 pb-4 border-b-2 border-gray-200">
+
+            <p className='text-[#131313]/70'>{eachPlayer.country}</p>
+            <button className='btn'>{eachPlayer.role}</button>
+        </div>
+        <div className="flex justify-between items-center">
+            <span className='font-bold'>Rating</span>
+            <h3 className='text-[#131313]/70'>{eachPlayer.rating}</h3>
+        </div>
+        <div className="flex justify-between items-center mb-3">
+            <h3 className='font-bold' >{eachPlayer.battingStyle}</h3>
+            <h3 className='text-[#131313]/70'>{eachPlayer.bowlingStyle}</h3>
+        </div>
+        <div className="flex justify-between items-center">
+            <h3 className='font-bold'>Price:{eachPlayer.price}</h3>
+            <button className='btn bg-white'>Choose Player</button>
+        </div>
         </div>
     );
 };
--- src/components/players/Players.jsx
+++ src/components/players/Players.jsx
@@ -5,7 +5,7 @@ import Player from '../player/Player';
 const Players = ({playersPromise}) => {
     const promiseData = use(playersPromise)
     return (
-        <div>
+        <div className='w-11/12 mx-auto grid grid-cols-3 gap-8'>
         {
             promiseData.map(eachPlayer => <Player key={eachPlayer.id} eachPlayer={eachPlayer} ></Player> )
         }
